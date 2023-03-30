8e3f4859fcc169776f33f815e738870c5c33863f
hhuezo <hugo.alex.huezo@gmail.com>
2023-03-29 19:45:13 -0600

    modelo de ruta

---
+++ app/Models/catalogo/Ruta.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models\catalogo;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ruta extends Model
+{
+    use HasFactory;
+    protected $table = 'tipo_negocio';
+
+    protected $primaryKey = 'Id';
+
+    public $timestamps = false;
+
+
+    protected $fillable = [
+        'Codigo',
+        'Nombre',
+        'Activo'
+    ];
+
+    protected $guarded = [];
+}
