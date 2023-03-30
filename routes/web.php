b166629b8ed9dba3550df1842cd9d02a68b20ae6
hhuezo <hugo.alex.huezo@gmail.com>
2023-03-29 22:12:44 -0600

    ruta aseguradora

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\seguridad\UserController;
 use App\Http\Controllers\catalogo\ClienteController;
-
+use App\Http\Controllers\catalogo\AseguradoraController;
 
 /*
 |--------------------------------------------------------------------------
