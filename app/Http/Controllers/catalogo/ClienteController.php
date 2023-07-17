607c53071b6917932ad1f67b1389da0e4639cee9
jramirez007 <jerf07@yahoo.com>
2023-07-16 21:00:05 -0600

    Implementando crear cliente

--- app/Http/Controllers/catalogo/ClienteController.php
+++ app/Http/Controllers/catalogo/ClienteController.php
@@ -4,9 +4,13 @@ namespace App\Http\Controllers\catalogo;
 
 use App\Http\Controllers\Controller;
 use App\Models\catalogo\Cliente;
+use App\Models\catalogo\ClienteEstado;
 use App\Models\catalogo\Ruta;
 use App\Models\catalogo\TipoContribuyente;
 use App\Models\catalogo\UbicacionCobro;
+use App\Models\polizas\Deuda;
+use App\Models\polizas\Residencia;
+use App\Models\polizas\Vida;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
 use RealRashid\SweetAlert\Facades\Alert;
@@ -22,12 +26,13 @@ class ClienteController extends Controller
 
     public function create()
     {
-        //alert()->success('El registro ha sido agregado correctamente');
+
         $tipos_contribuyente = TipoContribuyente::get();
         $rutas = Ruta::where('Activo','=',1)->get();
         $ubicaciones_cobro = UbicacionCobro::where('Activo','=',1)->get();
+        $cliente_estados = ClienteEstado::get();
 
-        return view('catalogo.cliente.create',compact( 'tipos_contribuyente','rutas','ubicaciones_cobro'));
+        return view('catalogo.cliente.create',compact( 'tipos_contribuyente','rutas','ubicaciones_cobro','cliente_estados'));
     }
 
     public function store(Request $request)
@@ -38,29 +43,46 @@ class ClienteController extends Controller
         $cliente = new Cliente();
         $cliente->Nit = $request->get('Nit');
         $cliente->Dui = $request->get('Dui');
+
+        $cliente->RegistroFiscal = $request->get('RegistroFiscal');
         $cliente->Nombre = $request->get('Nombre');
+        $cliente->FechaNacimiento = $request->get('FechaNacimiento');
+        $cliente->EstadoFamiliar = $request->get('EstadoFamiliar');
+        $cliente->NumeroDependientes = $request->get('NumeroDependientes');
+        $cliente->Ocupacion = $request->get('Ocupacion');
         $cliente->DireccionResidencia = $request->get('DireccionResidencia');
         $cliente->DireccionCorrespondencia = $request->get('DireccionCorrespondencia');
-        $cliente->TelefonoResidencia = $request->get('TelefonoResidencia');
+        $cliente->TelefonoRecidencia = $request->get('TelefonoRecidencia');
+
         $cliente->TelefonoOficina = $request->get('TelefonoOficina');
         $cliente->TelefonoCelular = $request->get('TelefonoCelular');
-        $cliente->Correo = $request->get('Correo');
-        $cliente->Ruta = $request->get('Ruta');
+        $cliente->CorreoPrincipal = $request->get('CorreoPrincipal');
+        $cliente->CorreoSecundario = $request->get('CorreoSecundario');
+        $cliente->FechaVinculacion = $request->get('FechaVinculacion');
+        $cliente->FechaBaja = $request->get('FechaBaja');
+        //$cliente->Ruta = $request->get('Ruta');
+
         $cliente->ResponsablePago = $request->get('ResponsablePago');
         $cliente->TipoContribuyente = $request->get('TipoContribuyente');
         $cliente->UbicacionCobro = $request->get('UbicacionCobro');
-        $cliente->Contacto = $request->get('Contacto');
+
+        // //$cliente->Contacto = $request->get('Contacto');
         $cliente->Referencia = $request->get('Referencia');
-        $cliente->NumeroTarjeta = $request->get('NumeroTarjeta');
-        $cliente->FechaVencimiento = $request->get('FechaVencimiento');
+        //$cliente->NumeroTarjeta = $request->get('NumeroTarjeta');
+        //$cliente->FechaVencimiento = $request->get('FechaVencimiento');
+        $cliente->Estado = $request->get('Estado');
         $cliente->Genero = $request->get('Genero');
         $cliente->TipoPersona = $request->get('TipoPersona');
-        $cliente->FechaCreacion = $time->toDateTimeString();
-        $cliente->UsuarioCreacion = auth()->user()->id;
+        $cliente->FechaIngreso = $time->toDateTimeString();
+        $cliente->UsuarioIngreso = auth()->user()->id;
+
         $cliente->save();
 
         alert()->success('El registro ha sido creado correctamente');
-        return back();
+
+        return redirect('catalogo/cliente/'.$cliente->Id.'/edit');
+
+        //return back();
     }
 
     public function cliente_create(Request $request)
@@ -101,7 +123,15 @@ class ClienteController extends Controller
         $tipos_contribuyente = TipoContribuyente::get();
         $rutas = Ruta::where('Activo','=',1)->get();
         $ubicaciones_cobro = UbicacionCobro::where('Activo','=',1)->get();
-        return view('catalogo.cliente.edit',compact( 'cliente','tipos_contribuyente','rutas','ubicaciones_cobro'));
+        $cliente_estados = ClienteEstado::get();
+        $action = 1;
+
+        $poliza_deudas = Deuda::get();
+        $poliza_residencias = Residencia::get();
+        $poliza_vidas = Vida::get();
+
+        return view('catalogo.cliente.edit',compact( 'tipos_contribuyente','rutas','ubicaciones_cobro','cliente_estados',
+                'cliente','action','poliza_deudas','poliza_residencias','poliza_vidas'));
 
     }
 
---
+++ app/Models/catalogo/ClienteEstado.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models\catalogo;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ClienteEstado extends Model
+{
+    use HasFactory;
+    protected $table = 'cliente_estado';
+
+    protected $primaryKey = 'Id';
+
+    public $timestamps = false;
+
+
+    protected $fillable = [
+        'nombre',
+    ];
+
+    protected $guarded = [];
+}
