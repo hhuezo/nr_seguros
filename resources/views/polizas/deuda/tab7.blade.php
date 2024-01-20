ac63173682a1433d958a8693b9245619b6174437
hhuezo <hugo.alex.huezo@gmail.com>
2024-01-20 12:59:14 -0600

    agregar extraprimados

--- resources/views/polizas/deuda/tab7.blade.php
+++ resources/views/polizas/deuda/tab7.blade.php
@@ -20,15 +20,19 @@
                 </thead>
                 <tbody>
                     @foreach ($clientes as $obj)
-                    @if ($clientes->count() > 0)
-                    <tr>
-                        <td><input type="checkbox"></td>
-                        <td>{{$obj->Dui}}</td>
-                        <td>{{$obj->PrimerNombre}} {{$obj->PrimerApellido}}</td>
-                        <td>{{$obj->NumeroReferencia}}</td>
-                        <td>{{$obj->SaldoTotal}}</td>
-                    </tr>
-                    @endif
+                        @if ($clientes->count() > 0)
+                            <tr>
+                                <td align="center"><button class="btn btn-primary"
+                                        onclick="modalExtraprimados({{ $deuda->Id }},'{{ $obj->Dui }}')"
+                                        data-target="#modal_extraprimados" data-toggle="modal">
+                                        <i class="fa fa-edit fa-lg"></i>
+                                    </button></td>
+                                <td>{{ $obj->Dui }}</td>
+                                <td>{{ $obj->Nombre }}</td>
+                                <td>{{ $obj->NumeroReferencia }}</td>
+                                <td>{{ $obj->SaldoTotal }}</td>
+                            </tr>
+                        @endif
                     @endforeach
                 </tbody>
             </table>
@@ -41,7 +45,6 @@
 
 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-
 
         <div class="x_title">
             <h4>&nbsp;&nbsp; Listado de Extra Primados<small></small>
@@ -57,22 +60,58 @@
                         <th>Nombre</th>
                         <th>Numero Referencia</th>
                         <th>Saldo Total</th>
+                        <th>Tarifa</th>
+                        <th>Porcentaje EP</th>
+                        <th>Pago EP</th>
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach ($extraprimados as $obj)
                     @if ($extraprimados->count() > 0)
-                    <tr>
-                        <td><input type="checkbox"></td>
-                        <td>{{$obj->Dui}}</td>
-                        <td>{{$obj->PrimerNombre}} {{$obj->PrimerApellido}}</td>
-                        <td>{{$obj->NumeroReferencia}}</td>
-                        <td>{{$obj->SaldoTotal}}</td>
-                    </tr>
+                        @foreach ($extraprimados as $obj)
+                            <tr>
+                                <td align="center"><button class="btn btn-primary"
+                                    data-target="#modal-edit-extraprimados-{{ $obj->Id }}" data-toggle="modal">
+                                    <i class="fa fa-edit fa-lg"></i>
+                                </button></td>
+                                <td>{{ $obj->Dui }}</td>
+                                <td>{{ $obj->PrimerNombre }} {{ $obj->PrimerApellido }}</td>
+                                <td>{{ $obj->NumeroReferencia }}</td>
+                                <td>{{ $obj->SaldoTotal }}</td>
+                                <td>{{ $obj->Tarifa }}</td>
+                                <td>{{ $obj->PorcentajeEP }}%</td>
+                                <td>{{ $obj->PagoEP }}</td>
+                            </tr>
+                            @include('polizas.deuda.modal_edit_extraprimados')
+                        @endforeach
                     @endif
-                    @endforeach
                 </tbody>
             </table>
         </div>
     </div>
 </div>
+
+@include('polizas.deuda.modal_extraprimados')
+
+<script type="text/javascript">
+    function modalExtraprimados(poliza, dui) {
+        console.log(poliza, dui);
+        // Construir la URL con los parámetros
+        var url = "{{ url('polizas/deuda/get_extraprimado') }}" + '/' + poliza + '/' + dui;
+
+        // Realizar la solicitud GET
+        $.get(url, function(data) {
+                console.log(data);
+                document.getElementById('ExtraprimadosDui').value = data.Dui;
+                document.getElementById('ExtraprimadosNombre').value = data.Nombre;
+                document.getElementById('ExtraprimadosFechaOtorgamiento').value = data.FechaOtorgamiento;
+                document.getElementById('ExtraprimadosNumeroReferencia').value = data.NumeroReferencia;
+                if (data.hasOwnProperty('MontoOtorgamiento')) {
+                    document.getElementById('ExtraprimadosMontoOtorgamiento').value = data.MontoOtorgamiento;
+                }
+            })
+            .fail(function() {
+                // Manejar errores si es necesario
+                console.error("Error al realizar la solicitud GET.");
+            });
+    }
+</script>
