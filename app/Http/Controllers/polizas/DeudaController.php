c6881127abdabb160e9a13d0565249ae468e660d
Mar Escobar <mar.escobar1991@gmail.com>
2024-03-03 17:37:25 -0600

    agrega credito

--- app/Http/Controllers/polizas/DeudaController.php
+++ app/Http/Controllers/polizas/DeudaController.php
@@ -1848,7 +1848,7 @@ class DeudaController extends Controller
 
     public function agregar_valido(Request $request)
     {
-        $poliza = PolizaDeudaTempCartera::where('NumeroReferencia','=',$request->id)->first();
+        $poliza = PolizaDeudaTempCartera::findOrFail($request->id);
         $poliza->NoValido = 0;
         $poliza->update();
 
