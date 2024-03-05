3cfcf8954b93ddd2a54c875f1f5b329ec936380a
Mar Escobar <mar.escobar1991@gmail.com>
2024-03-04 20:42:59 -0600

    observaciones 2

--- app/Http/Controllers/polizas/DeudaController.php
+++ app/Http/Controllers/polizas/DeudaController.php
@@ -1998,7 +1998,12 @@ class DeudaController extends Controller
                 ->groupBy('Dui')
                 ->get();
 
-            dd($poliza_cumulos->take(10));
+                $poliza_cumulos = PolizaDeudaTempCartera::selectRaw('Id,Dui,Edad,Nit,PrimerNombre,SegundoNombre,PrimerApellido,SegundoApellido,ApellidoCasada,FechaNacimiento, 
+        NumeroReferencia,NoValido,Perfiles,EdadDesembloso,FechaOtorgamiento,NoValido,
+         GROUP_CONCAT(DISTINCT NumeroReferencia SEPARATOR ", ") AS ConcatenatedNumeroReferencia,SUM(SaldoCapital) as total_saldo,SUM(Intereses) as total_interes,SUM(InteresesCovid) as total_covid,
+         SUM(InteresesMoratorios) as total_moratorios, SUM(MontoNominal) as total_monto_nominal')->groupBy('Dui', 'NoValido')->get();
+
+         //   dd($poliza_cumulos->take(10));
         }
 
 
