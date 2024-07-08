ed030dd492cdf8000755ecbbd522bfbc7f6505b6
Mar Escobar <mar.escobar1991@gmail.com>
2024-07-07 19:46:04 -0600

    cambios de excluidos

--- resources/views/polizas/deuda/tab1.blade.php
+++ resources/views/polizas/deuda/tab1.blade.php
@@ -33,7 +33,7 @@
                                     </div>
                                     <div class="modal-footer">
                                         <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
-                                        <button type="button" class="btn btn-primary">Confirmar</button>
+                                        <button type="submit" class="btn btn-primary">Confirmar</button>
                                     </div>
                                 </div>
                             </div>
