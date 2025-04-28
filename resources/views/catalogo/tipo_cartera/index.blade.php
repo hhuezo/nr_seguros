cdbf28ccfdea7285a254eb6a3289df1c1937b105
hhuezo <hulgo.alex.huezo@gmail.com>
2025-04-27 22:39:14 -0600

    crud permisos

--- resources/views/catalogo/tipo_cartera/index.blade.php
+++ resources/views/catalogo/tipo_cartera/index.blade.php
@@ -5,7 +5,7 @@
         @include('sweetalert::alert', ['cdn' => 'https://cdn.jsdelivr.net/npm/sweetalert2@9'])
         <div class="x_title">
             <div class="col-md-6 col-sm-6 col-xs-12">
-                <h3>Tipo de Cartera </h3>
+                <h3>Linea crédito (deuda) </h3>
             </div>
             <div class="col-md-6 col-sm-6 col-xs-12" align="right">
                 <a href="{{ url('catalogo/tipo_cartera/create/') }}"><button class="btn btn-info float-right"> <i
--- resources/views/seguridad/permission/edit.blade.php
+++ resources/views/seguridad/permission/edit.blade.php
@@ -1,56 +1,27 @@
-@extends ('welcome')
-@section('contenido')
-    @include('sweetalert::alert', ['cdn' => 'https://cdn.jsdelivr.net/npm/sweetalert2@9'])
+<div class="modal fade modal-slide-in-right" aria-hidden="true" role="dialog" tabindex="-1"
+    id="modal-edit-{{ $obj->id }}">
+    <form method="POST" action="{{ route('permission.update', $obj->id) }}">
+        @method('PUT')
+        @csrf
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
 
-    <div class="x_panel">
-        <div class="clearfix"></div>
-        <div class="row">
-            <div class="x_title">
-                <h2>Modificación de permiso</h2>
-                <ul class="nav navbar-right panel_toolbox">
+                    <h4 class="modal-title">Modificar registro</h4>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label class="control-label" align="right">Nombre</label>
 
-                </ul>
-                <div class="clearfix"></div>
-            </div>
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 form-horizontal form-label-left">
-
-
-                @if (count($errors) > 0)
-                    <div class="alert alert-danger">
-                        <ul>
-                            @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
+                        <input class="form-control" name="name" required type="text" value="{{ $obj->name }}">
                     </div>
-                @endif
-
-                <form method="POST" action="{{ route('permission.update', $permission->id) }}">
-                    @method('PUT')
-                    @csrf
-                    <div class="form-horizontal">
-                        <br>
-
-                        <div class="form-group row">
-                            <label class="control-label col-md-3" align="right">Nombre</label>
-                            <div class="col-md-6">
-                                <input class="form-control" name="name" required type="text"
-                                    value="{{ $permission->name }}" onblur="this.value = this.value.toLowerCase();">
-                            </div>
-                        </div>
-
-
-                        <div class="form-group" align="center">
-                            <button class="btn btn-success" type="submit">Guardar</button>
-                            <a href="{{ url('permission') }}"><button type="button"
-                                    class="btn btn-primary">Cancelar</button></a>
-                        </div>
-
-                    </div>
-
-                </form>
-
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+                    <button type="submit" class="btn btn-primary">Confirmar</button>
+                </div>
             </div>
         </div>
-    </div>
-@endsection
+    </form>
+
+</div>
