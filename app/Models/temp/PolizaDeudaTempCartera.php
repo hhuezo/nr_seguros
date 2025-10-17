2cbfb06213fc3c93480b679146cda0405b166d49
hhuezo <hulgo.alex.huezo@gmail.com>
2025-10-16 18:31:21 -0600

    linea credito

--- app/Models/temp/PolizaDeudaTempCartera.php
+++ app/Models/temp/PolizaDeudaTempCartera.php
@@ -125,7 +125,7 @@ class PolizaDeudaTempCartera extends Model
                     # .sando capital
                     //$saldo = $this->SaldoCapital;
 
-                    $saldo =  $this->SaldoCapital;
+                    $saldo =  0.00;
                     break;
             }
 
