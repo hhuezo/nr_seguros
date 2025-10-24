7988184fe550d92b10b1c673fa04cb335b01ca55
hhuezo <hugo.alex.huezo@gmail.com>
2025-10-24 13:17:39 -0600

    quitar editar vida

--- resources/views/polizas/vida/tab6.blade.php
+++ resources/views/polizas/vida/tab6.blade.php
@@ -46,9 +46,9 @@
                                 <a href="{{ url('poliza/vida/get_recibo') }}/{{ $obj->Id }}/1" target="_blank" class="btn btn-info"><span class="fa fa-print"></span></a>
                                 &nbsp;
                                 <!-- <a href="{{ url('poliza/vida/get_recibo') }}/{{ $obj->Id }}/2" target="_blank" class="btn btn-success"><span class="fa fa-file-excel-o fa-lg"></span></a> -->
-                                &nbsp;
+                                {{-- &nbsp;
                                 <a href="{{ url('poliza/vida/get_recibo_edit') }}/{{ $obj->Id }}" target="_blank" class="btn btn-warning"><span class="fa fa-pencil"></span></a>
-                                @endif
+                                @endif --}}
                         </td>
                     </tr>
                     @endif
