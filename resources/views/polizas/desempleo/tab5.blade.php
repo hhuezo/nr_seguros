1dfb6fd956a75b3bbf6c4c5a8cce0789a5038f5f
Your Name <hugo.alex.huezo@gmail.com>
2025-11-13 13:42:10 -0600

    blank recibo

--- resources/views/polizas/desempleo/tab5.blade.php
+++ resources/views/polizas/desempleo/tab5.blade.php
@@ -43,9 +43,7 @@
                         @endif
                         <td>
                             @if($obj->Activo <> 0)
-                                <a href="{{ url('poliza/desempleo/get_recibo') }}/{{ $obj->Id }}/1"  class="btn btn-info"><span class="fa fa-print"></span></a>
-                                &nbsp;
-                                <!-- <a href="{{ url('poliza/desempleo/get_recibo') }}/{{ $obj->Id }}/2" target="_blank" class="btn btn-success"><span class="fa fa-file-excel-o fa-lg"></span></a> -->
+                                <a href="{{ url('poliza/desempleo/get_recibo') }}/{{ $obj->Id }}/1" target="_blank"  class="btn btn-info"><span class="fa fa-print"></span></a>
                                 &nbsp;
                                 <a href="{{ url('poliza/desempleo/get_recibo_edit') }}/{{ $obj->Id }}" target="_blank" class="btn btn-warning"><span class="fa fa-pencil"></span></a>
                                 @endif
